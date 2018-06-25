09bbb5a3dd31d5b03796395b93e18f986604b739
Jim Kiffmeyer <kiffmejs@mail.uc.edu>
2018-06-25 14:38:26 -0400

    Chaning the First Header

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('It works!')
+const button = document.querySelector('button')
+
+const changeHeading = function() {
+    const heading = document.querySelector('h1')
+    heading.textContent = "Jim Kiffmeyer\'s Page"
+}
+
+button.addEventListener('click', changeHeading)
