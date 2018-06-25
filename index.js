24b92e25e90e1aad2a7623f6e8eaa8c62ebce304
Jim Kiffmeyer <kiffmejs@mail.uc.edu>
2018-06-25 14:59:06 -0400

    Changing the Second Header

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const button = document.querySelector('button')
 
 const changeHeading = function() {
-    const heading = document.querySelector('h1')
-    heading.textContent = "Jim Kiffmeyer\'s Page"
+    const heading = document.querySelector('#SecondHeading')
+    heading.textContent = "Downtown Indy"
 }
 
 button.addEventListener('click', changeHeading)
