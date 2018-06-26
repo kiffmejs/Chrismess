b2c58510bec9574f29b74e82f94213d8b47fd973
Jim Kiffmeyer <kiffmejs@mail.uc.edu>
2018-06-26 16:31:35 -0400

    Add Div

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const form = document.querySelector('form#flickForm')
 const changeHeading = function(ev) {
   ev.preventDefault()
   const f = ev.target
-  const heading = document.querySelector('h1')
-  heading.textContent = f.flickName.value
-
+  const flicksDiv = document.querySelector("#flicks")
+  flicksDiv.textContent += ' ' + f.flickName.value
 }
 
 form.addEventListener('submit', changeHeading)
+
+
 
