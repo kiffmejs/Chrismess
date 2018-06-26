b31f42d9555850b84131cbc5ba5a4959e0ae3e89
Jim Kiffmeyer <kiffmejs@mail.uc.edu>
2018-06-26 17:00:42 -0400

    Add Unordered List

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const changeHeading = function(ev) {
   ev.preventDefault()
   const f = ev.target
   const flickName = f.flickName.value
-  const flicksDiv = document.querySelector("#flicks")
-  flicksDiv.innerHTML += `<p>${flickName}</p>`
+  const list = document.querySelector("#flicks")
+  list.innerHTML += `<li>${flickName}</li>`
 
   f.reset()
 }
