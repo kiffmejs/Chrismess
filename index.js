8932b5cf5d17119a5151e8fc606081a3eb59391d
Jim Kiffmeyer <kiffmejs@mail.uc.edu>
2018-06-27 10:25:47 -0400

    HW 2 : Part 1 with the feilds (Correct)

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const form = document.querySelector('form#flickForm')
 const movieSubmit = function(ev) {
   ev.preventDefault()
   const f = ev.target
+
   const flickName = f.flickName.value
+  const flickSpan = document.createElement('span')
+  flickSpan.textContent = flickName
+
+  const flickYear = f.flickYear.value
+  const yearSpan = document.createElement('span')
+  yearSpan.textContent = flickYear
+
   const item = document.createElement ('li')
-  item.textContent = flickName
+  item.appendChild(flickSpan)
+  item.appendChild(yearSpan) 
+
   const list = document.querySelector("#flicks")
   list.appendChild(item)
 
@@ -14,15 +24,4 @@ const movieSubmit = function(ev) {
 
 form.addEventListener('submit', movieSubmit)
 
-const tvSubmit = function (ev){
-  ev.preventDefault()
-  const t = ev.target
-  const tvShow = t.tvShow.value
-  const item = document.createElement('li')
-  item.textContent = tvShow
-  const list = document.querySelector ("#flicks")
-  tvlist.appendChild(item2)
-}
-
-form.addEventListener('submit', tvSubmit)
 
