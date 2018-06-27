51404c65ed6298397d4bffce5e1f8e9122ca10d1
Jim Kiffmeyer <kiffmejs@mail.uc.edu>
2018-06-27 16:52:46 -0400

    Use an ES6 class and arrow functions

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
-const form = document.querySelector('form#flickForm')
+class App {
+  constructor(){
+    const form = document.querySelector('form#flickForm')
+    form.addEventListener('submit', (ev) => {
+      ev.preventDefault()
+      this.movieSubmit(ev)
+    })
+  }
+ renderProperty(name, value) {
+    const Span = document.createElement('span')
+    Span.classList.add(name)
+    Span.textContent = value
+    return Span
+  }
+  
+  renderItem(flick) {
+    const item = document.createElement ('li')
+    item.classList.add('flick')
+  
+    const properties = Object.keys(flick)
 
-const renderProperty = function (name, value) {
-  const Span = document.createElement('span')
-  Span.classList.add(name)
-  Span.textContent = value
-  return Span
+    properties.forEach((propertyName) => {
+      const span = this.renderProperty(propertyName, flick[propertyName])
+      item.appendChild(span)
+    })
+  
+    return item
+  
+  }
+  
+ movieSubmit(ev) {
+    ev.preventDefault()
+    const f = ev.target
+  
+  const flick = {
+    name: f.flickName.value,
+    year: f.flickYear.value,
+  }
+  
+   const item = this.renderItem(flick)
+  
+    const list = document.querySelector("#flicks")
+    list.appendChild(item)
+  
+    f.reset()
+  }
 }
 
-const renderItem = function(flick) {
-  const item = document.createElement ('li')
-  item.classList.add('flick')
+const app = new App()
 
-  const properties = object.keys(flick)
-  properties.forEach(function(propertyName){
-    const span = renderProperty(propertyName, flick[propertyName])
-    item.appendChild(span)
-  })
 
-  return item
 
-}
 
-const movieSubmit = function(ev) {
-  ev.preventDefault()
-  const f = ev.target
 
-const flick = {
-  name: f.flickName.value,
-  year: f.flickYear.value,
-}
 
-  const flickSpan = renderProperty('name', f.flickName.value)
-  const yearSpan = renderProperty('year', f.flickYear.value)
-
-  const item = document.createElement ('li')
-  item.classList.add('flick')
-  item.appendChild(flickSpan)
-  item.appendChild(yearSpan) 
-
-  const list = document.querySelector("#flicks")
-  list.appendChild(item)
-
-  f.reset()
-}
-
-form.addEventListener('submit', movieSubmit)
 
 
