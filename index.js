71da7eada01e4b81ab4399941df4c316fcbb58ef
Jim Kiffmeyer <kiffmejs@mail.uc.edu>
2018-06-27 10:49:15 -0400

    Changed year to be in a different line using "classList"

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const movieSubmit = function(ev) {
 
   const flickName = f.flickName.value
   const flickSpan = document.createElement('span')
+  flickSpan.classList.add('name')
   flickSpan.textContent = flickName
 
   const flickYear = f.flickYear.value
   const yearSpan = document.createElement('span')
+  flickSpan.classList.add('year')
   yearSpan.textContent = flickYear
 
   const item = document.createElement ('li')
+  item.classList.add('flick')
   item.appendChild(flickSpan)
   item.appendChild(yearSpan) 
 
