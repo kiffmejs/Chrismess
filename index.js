090f9bd11e6075ccc21fd6a17064911cc2b353d3
Jim Kiffmeyer <kiffmejs@mail.uc.edu>
2018-06-28 10:47:25 -0400

    Add Delete Button and Remove From Arrat

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 class App {
   constructor(){
+    this.flicks = []
+    this.list = document.querySelector("#flicks")
     const form = document.querySelector('form#flickForm')
     form.addEventListener('submit', (ev) => {
       ev.preventDefault()
@@ -23,11 +25,28 @@ class App {
       const span = this.renderProperty(propertyName, flick[propertyName])
       item.appendChild(span)
     })
+
+      const deleteButton = document.createElement('button')
+      deleteButton.textContent = 'delete'
+      deleteButton
+      .addEventListener (
+        'click',
+         (_ev) => this.removeFlick(flick, item)
+        )
+      item.appendChild(deleteButton)
+
   
     return item
   
   }
-  
+
+  removeFlick(flick, item) {
+    this.list.removeChild(item)
+
+    const i = this.flicks.indexOf(flick)
+    this.flicks.splice(i,1)
+  }
+
  movieSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
@@ -36,14 +55,15 @@ class App {
     name: f.flickName.value,
     year: f.flickYear.value,
   }
+
+  this.flicks.push(flick)
   
    const item = this.renderItem(flick)
-  
-    const list = document.querySelector("#flicks")
-    list.appendChild(item)
+    this.list.appendChild(item)
   
     f.reset()
     f.flickName.focus()
+    
   }
 }
 
